75383f94441a0aae7bc7534bef3aa35f6bbcc4dd
VladFalcon <ZzZ05658@gmail.com>
2020-08-05 20:37:35 +0300

    described classes DAO, AccountDao and CustomerDao

--- src/main/java/com/falcon/clientbank/dao/AccountDao.java
+++ src/main/java/com/falcon/clientbank/dao/AccountDao.java
@@ -1,4 +1,60 @@
 package com.falcon.clientbank.dao;
 
-public class AccountDao {
+import com.falcon.clientbank.enteties.Account;
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Repository
+public class AccountDao implements Dao<Account>{
+
+    private final List<Account> accounts = new ArrayList<>();
+
+    @Override
+    public Account save(Account obj) {
+        accounts.add((Account) obj);
+        return (Account) obj;
+    }
+
+    @Override
+    public boolean delete(Account obj) {
+        return accounts.remove((Account) obj);
+    }
+
+    @Override
+    public void deleteAll(List<Account> entities) {
+        accounts.clear();
+    }
+
+    @Override
+    public void saveAll(List<Account> entities) {
+        accounts.containsAll(entities);
+    }
+
+    @Override
+    public List<Account> findAll() {
+        return accounts;
+    }
+
+    @Override
+    public boolean deleteById(long id) {
+        for(int i = 0; i < accounts.size(); i++){
+            if(accounts.get(i).getId() == id){
+                accounts.remove(accounts.get(i));
+                return true;
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public Account getOne(long id) {
+        for(int i = 0; i < accounts.size(); i++){
+            if(accounts.get(i).getId() == id){
+                return accounts.get(i);
+            }
+        }
+        return null;
+    }
 }
--- src/main/java/com/falcon/clientbank/dao/CustomerDao.java
+++ src/main/java/com/falcon/clientbank/dao/CustomerDao.java
@@ -1,4 +1,75 @@
 package com.falcon.clientbank.dao;
 
-public class CustomerDao {
+import com.falcon.clientbank.enteties.Account;
+import com.falcon.clientbank.enteties.Customer;
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public class CustomerDao implements Dao<Customer>{
+    private List<Customer> customers = new ArrayList<>();
+
+    @Override
+    public Customer save(Customer customer) {
+        customers.add(customer);
+        return customer;
+    }
+
+    public boolean update(Customer customer) {
+        customers.stream().filter(el -> el.getId().equals(customer.getId())).findFirst().ifPresent(el -> {
+            el.setName(customer.getName());
+            el.setEmail(customer.getEmail());
+            el.setAge(customer.getAge());
+        });
+
+        return true;
+    }
+
+    @Override
+    public boolean delete(Customer customer) {
+        return customers.remove(customer);
+    }
+
+    @Override
+    public void deleteAll(List<Customer> customers) {
+
+    }
+
+    @Override
+    public void saveAll(List<Customer> customers) {
+
+    }
+
+    @Override
+    public List<Customer> findAll() {
+        return customers;
+    }
+
+    @Override
+    public boolean deleteById(long id) {
+        return false;
+    }
+
+    @Override
+    public Customer getOne(long id) {
+        return customers.get((int) id);
+    }
+
+    public Account createAccount(Account account, Long id) {
+        customers.stream()
+                .filter(el -> el.getId().equals(id))
+                .findFirst()
+                .ifPresent(el -> el.getAccounts().add(account));
+        return account;
+    }
+
+    public boolean deleteAccount(Account account, Long id) {
+        Optional<Customer> optionalCustomer = customers.stream()
+                .filter(el -> el.getId().equals(id))
+                .findFirst();
+        return optionalCustomer.get().getAccounts().remove(account);
+    }
 }
