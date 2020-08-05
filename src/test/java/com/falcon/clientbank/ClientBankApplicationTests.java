132721378e81498a3de649e574a733998032dd7c
VladFalcon <ZzZ05658@gmail.com>
2020-08-05 20:09:17 +0300

    Initial commit

---
+++ src/test/java/com/falcon/clientbank/ClientBankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.falcon.clientbank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
