14918ce8652c5c72cb2b735793a472418b2b365b
VladFalcon <ZzZ05658@gmail.com>
2020-08-12 19:43:32 +0300

    added UI components

---
+++ src/main/webapp/client-bank-ui/src/store/account/slice.js
@@ -0,0 +1,4 @@
+import {createAsyncThunk, createSlice} from "@reduxjs/toolkit";
+import {status} from "../status";
+import Api from "../../services/Api";
+import {customerSlice} from "../customer/slice";
